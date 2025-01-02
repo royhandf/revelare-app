ea9f3736ef5838f2700628d1d80908719b6cd740
royhandf <royhandaffa14@gmail.com>
2025-01-02 11:32:48 +0700

    feat: fetch data

---
+++ src/utils/fetch.js
@@ -0,0 +1,168 @@
+import axios from "axios";
+import { config } from "../configs";
+
+export async function searchBooks(query, page = 1) {
+  try {
+    const response = await axios.get(
+      `${config.api_host}/books/search?query=${query}&page=${page}`
+    );
+
+    if (response.data.status === "success") {
+      return response.data;
+    } else {
+      throw new Error(response.data.message || "Something went wrong");
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getBookById(id) {
+  try {
+    const response = await axios.get(`${config.api_host}/books/${id}`, {
+      withCredentials: true,
+    });
+
+    if (response.data.status === "success") {
+      return response.data;
+    } else {
+      throw new Error(response.data.message || "Something went wrong");
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function signup(data) {
+  try {
+    const response = await axios.post(`${config.api_host}/signup`, {
+      ...data,
+    });
+
+    if (response.data.status === "success") {
+      const { token, user } = response.data;
+      localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(user));
+
+      return response.data;
+    } else {
+      throw new Error(response.data.message || "Failed to register");
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function signin(email, password) {
+  try {
+    const response = await axios.post(`${config.api_host}/signin`, {
+      email,
+      password,
+    });
+
+    if (response.data.status === "success") {
+      const { token, user } = response.data;
+      localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(user));
+
+      return response.data;
+    } else {
+      throw new Error(response.data.message || "Something went wrong");
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getData(page = 1) {
+  try {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      throw new Error("Unauthorized");
+    }
+
+    const response = await axios.get(
+      `${config.api_host}/dashboard/books?page=${page}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (response.data.status === "success") {
+      return response.data;
+    } else {
+      throw new Error(response.data.message || "Failed to fetch books");
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function addBook(data) {
+  try {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      throw new Error("Unauthorized");
+    }
+
+    const response = await axios.post(
+      `${config.api_host}/dashboard/book/create`,
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (response.data.status === "success") {
+      return response.data;
+    } else {
+      throw new Error(response.data.message || "Failed to add book");
+    }
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw new Error(error.response.data.message || "An error occurred");
+    } else {
+      throw new Error(error.message || "An unknown error occurred");
+    }
+  }
+}
+
+export async function deleteBook(id) {
+  try {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      throw new Error("Unauthorized");
+    }
+
+    const response = await axios.delete(
+      `${config.api_host}/dashboard/book/delete/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (response.data.status === "success") {
+      return response.data;
+    } else {
+      throw new Error(response.data.message || "Failed to delete book");
+    }
+  } catch (error) {
+    throw error;
+  }
+}

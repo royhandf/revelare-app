797f1ba660eacf13362db5489a5443f7ee36c0cd
royhandf <royhandaffa14@gmail.com>
2025-01-02 12:26:22 +0700

    fix: access role

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import DropdownItem from "./DropdownItem";
 
 const Dropdown = ({ onSignOut }) => {
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+
   return (
     <div className="absolute right-0 z-10 mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
       <ul className="py-2 text-sm text-gray-700">
-        <DropdownItem to="/dashboard/books">Dashboard</DropdownItem>
-        <DropdownItem to="/bookmarks">Bookmarks</DropdownItem>
+        {currentUser && currentUser.role === "admin" && (
+          <DropdownItem to="/dashboard/books">Dashboard</DropdownItem>
+        )}
+        <DropdownItem to="/bookmark">Bookmark</DropdownItem>
         <DropdownItem to="/" onClick={onSignOut}>
-          Sign out
+          Log out
         </DropdownItem>
       </ul>
     </div>
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,8 @@
 const PrivateRoute = ({ element, ...rest }) => {
-  const currentUser = JSON.parse(localStorage.getItem("user")); // Mengambil user dari localStorage
+  const currentUser = JSON.parse(localStorage.getItem("user"));
 
   if (!currentUser || currentUser.role !== "admin") {
+    console.log(currentUser.role);
     return (window.location.href = "/error");
   }
 

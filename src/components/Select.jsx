fbb1d6e3084b542ecae7120728c05019640ecad5
royhandf <royhandaffa14@gmail.com>
2025-01-14 07:27:33 +0700

    fix: selected item

---
+++ src/components/Select.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Select = ({ id, options, value, onChange }) => {
+  return (
+    <select
+      id={id}
+      value={value}
+      onChange={onChange}
+      className="text-sm text-gray-700 ps-3 bg-white divide-y divide-gray-100 shadow-sm w-48 rounded-s-full rounded-e-none border border-gray-300 focus:ring-1 focus:ring-violet-500 focus:border-violet-500 focus:outline-none focus:z-10"
+    >
+      {options.map((option, index) => (
+        <option
+          key={index}
+          value={option.value}
+          disabled={option.disabled}
+          selected={option.selected}
+        >
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
